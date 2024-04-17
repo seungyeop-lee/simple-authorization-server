b72f926245362e2165db98451ff7c17aae5a0cda
seungyeop-lee <xbeast7@gmail.com>
2024-04-17 19:50:47 +0900

    frontend/auth-client: react router 제거 및 목 페이지 생성
    auth-server에서 서빙 예정이므로, router를 제거

--- frontend/auth-client/src/main.tsx
+++ frontend/auth-client/src/main.tsx
@@ -1,23 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Root from "./routes/Root.tsx";
 import {StyledEngineProvider, ThemeProvider} from "@mui/material";
 import theme from "./components/theme.ts";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root/>,
-    },
-]);
+import Login from "./routes/Login.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <StyledEngineProvider injectFirst>
             <ThemeProvider theme={theme}>
-                <RouterProvider router={router}/>
+                <Login />
             </ThemeProvider>
         </StyledEngineProvider>
     </React.StrictMode>,
